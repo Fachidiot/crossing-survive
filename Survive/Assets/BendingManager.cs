789902b09888e4fa1cfb55ab79531cb91de01964
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2022-12-05 20:34:47 +0900

    blender

--- Survive/Assets/BendingManager.cs
+++ Survive/Assets/BendingManager.cs
@@ -4,24 +4,27 @@ using UnityEngine.Rendering;
 [ExecuteAlways] // 에디트 모드 나 플레이 모드일때 항상 실행
 public class BendingManager : MonoBehaviour
 {
+    static float cullingDepth = 180;
+
     #region Constants
     const string BENDING_FEATURE = "ENABLE_BENDING";
     #endregion
 
     #region  MonoBehaviour
-    void Awake() {
+    void Awake()
+    {
         if (Application.isPlaying)
             Shader.EnableKeyword(BENDING_FEATURE);
         else
             Shader.DisableKeyword(BENDING_FEATURE);
     }
-    
+
     void OnEnable()
     {
         RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
         RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
     }
-    
+
     void OnDisable()
     {
         RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
@@ -32,11 +35,13 @@ public class BendingManager : MonoBehaviour
 
     #region Methods
 
-    static void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera) {
-        camera.cullingMatrix = Matrix4x4.Ortho(-99, 99, -99, 99, 0.001f, 99) * camera.worldToCameraMatrix;
+    static void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        camera.cullingMatrix = Matrix4x4.Ortho(-cullingDepth, cullingDepth, -cullingDepth, cullingDepth, 0.002f, cullingDepth) * camera.worldToCameraMatrix;
     }
 
-    static void OnEndCameraRendering(ScriptableRenderContext context, Camera camera) {
+    static void OnEndCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
         camera.ResetCullingMatrix();
     }
 
